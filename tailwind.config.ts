fcf11655239c2f37aac3ee4bd24bd8c6c539c5d9
Alessio Lagreca <82826676+AlessioLagreca@users.noreply.github.com>
2024-02-27 21:55:14 +0100

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        Cyan: "hsl(180, 66%, 49%)",
+        Violet: "hsl(257, 27%, 26%)",
+        DarkViolet: "hsl(260, 8%, 14%)",
       },
     },
   },
